eb90ec75776e18d52ff2c39cd0a22c9f1ffbcb8e
Yash-Sukhdeve <sukhdeveyash@gmail.com>
2024-04-03 16:54:21 -0400

    error resolved in app.py
    The error was that variable mf was used twice. Once as an object and another time as a file path.

--- app/app.py
+++ app/app.py
@@ -9,13 +9,16 @@ from tables.product_family import *'''
 
 mf = masterfile()
 pf = product_family()
-# print(os.listdir())
-# print(os.getcwd())
+print(os.listdir())
+print(os.getcwd())
 # print(os.path.join(os.getcwd(), 'app', 'MOCK_DATA.csv'))
-mf = os.path.join(os.getcwd(), 'app', 'masterdata.csv')
+## Changed name of file path from mf to mf_data
+mf_data = os.path.join(os.getcwd(), 'app', 'masterdata.csv')  
 prodfam = os.path.join(os.getcwd(), 'app', 'prodfam_data.csv')
-
 dbfile = os.path.join(os.getcwd(), 'app', 'schema.db')
+print(mf_data)
+print(prodfam)
+print(dbfile)
 
 dbsetup.cleardb(dbfile)
 dbsetup.dbsetup(dbfile)
@@ -23,7 +26,7 @@ dbsetup.dbsetup(dbfile)
 mf.createtable(dbfile)
 pf.createtable(dbfile)
 
-mf.insertdata(mf, dbfile)
+mf.insertdata(mf_data, dbfile)
 pf.insertdata(prodfam, dbfile)
 '''
 masterfile.basiccheck(dbfile)
